6edc92ec02e1eef3a48d8b8e661c02dcd96cc22a
Maria Garzon <garzonmm@mcmaster.ca>
2021-04-08 09:15:52 -0400

    summary stats on suspension coils

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,3 +2,6 @@ mpg <- read.csv('/Users/Maria/Desktop/MechaCar_Statistical_Analysis/Data/MechaCa
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg))
 
+sus_coils <- read.csv('/Users/Maria/Desktop/MechaCar_Statistical_Analysis/Data/Suspension_Coil.csv', stringsAsFactors = F)
+total_summary <- sus_coils %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- sus_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
