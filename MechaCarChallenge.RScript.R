dc5b803738cccb528b669738a1828235dfe215fd
Maria Garzon <garzonmm@mcmaster.ca>
2021-04-08 09:39:49 -0400

    t-tests and visuals

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 sus_coils <- read.csv('/Users/Maria/Desktop/MechaCar_Statistical_Analysis/Data/Suspension_Coil.csv', stringsAsFactors = F)
 total_summary <- sus_coils %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- sus_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+
+t.test(x=sus_coils$PSI, mu=1500)
+pop1 <- subset(sus_coils, Manufacturing_Lot=='Lot1')
+t.test(x=pop1$PSI, mu=1500)
+pop2 <- subset(sus_coils, Manufacturing_Lot=='Lot2')
+t.test(x=pop2$PSI, mu=1500)
+pop3 <- subset(sus_coils, Manufacturing_Lot=='Lot3')
+t.test(x=pop3$PSI, mu=1500)
